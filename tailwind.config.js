1938b5bd2df36c1ff4d5e530f061d202882271fe
Julio Andres Ramirez De Freitas <julio157ramirez@gmail.com>
2020-05-20 19:11:27 -0400

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+      poppins: ['Poppins'],
+    },
+    extend: {
+      colors: {
+        'fish-background-lightGray': '#F6F7FB',
+        'fish-text-primary': '#001567',
+        'fish-text-small': '#828282',
+        'fish-button-default': '#EFEFEF',
+        'fish-button-selected': '#22F2FF',
+        'fish-accent-cian': '#79FFE1',
+        'fish-accent-blue': '#5850EC',
+        'fish-accent-red': '#FF0080',
+        'fish-accent-green': '#3DF165',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
